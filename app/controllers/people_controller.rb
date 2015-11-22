64563a5c08bb62faac4e3c177b7b14800da3a7d4
Becky Bair <beckyhowen@gmail.com>
2015-11-22 14:57:28 -0600

    initial commit

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,90 @@
+
+get '/people' do
+  @people = Person.all
+  erb :"/people/index"
+end
+
+get '/people/new' do
+  @person = Person.new
+  erb :"/people/new"
+end
+
+post '/people' do
+	module Dates
+		def self.check_date(date)
+			begin
+				return birthdate = self.format_date(date)
+				rescue
+				return false
+			end
+
+		end
+
+		def self.format_date(date)
+			if date.include?("-")
+				birthdate = date
+			else
+				birthdate = Date.strptime(date, "%m%d%Y")
+			end
+		end
+	end
+  if Dates.check_date(params[:birthdate]) == false
+		birthdate = ""
+  else
+		birthdate = Dates.format_date(params[:birthdate])
+  end
+  
+#  if params[:birthdate].include?("-")
+#    birthdate = params[:birthdate]
+#  elsif
+#    !params[:birthdate].empty?
+#		birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+#	else
+#		"The Birthdate is empty"
+#  end
+  
+  person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+  if @person.valid?
+    @person.save
+    redirect "/people/#{@person.id}"
+  else
+    @person.errors.full_messages.each do |msg|
+      @errors = "#{@errors} #{msg}."
+    end
+    erb :"/people/new"
+  end
+end
+
+get '/people/:id/edit' do
+  @person = Person.find(params[:id])
+  erb :"/people/edit"
+end
+
+put '/people/:id' do
+  @person = Person.find(params[:id])
+  @person.first_name = params[:first_name]
+  @person.last_name = params[:last_name]
+  @person.birthdate = params[:birthdate]
+  if @person.valid?
+    @person.save
+    redirect "/people/#{@person.id}"
+  else
+    @person.errors.full_messages.each do |msg|
+      @errors = "#{@errors} #{msg}."
+    end
+    erb :"/people/edit"    
+  end
+end
+
+delete '/people/:id' do
+  person = Person.find(params[:id])
+  person.delete
+  redirect "/people"
+end
+
+get '/people/:id' do
+  @person = Person.find(params[:id])
+  birth_path_num = Person.get_birth_path_num(@person.birthdate.strftime("%m%d%Y"))
+  @message = Person.get_message(birth_path_num)  
+  erb :"/people/show"
+end
